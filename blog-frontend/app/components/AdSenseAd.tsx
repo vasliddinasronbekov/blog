d7d885cbaa8f698a07f36f5f28c6631a74567395
Windows User <asronbekovvasliddin@gmail.com>
2026-02-22 07:26:14 -0500

    .

--- blog-frontend/app/components/AdSenseAd.tsx
+++ blog-frontend/app/components/AdSenseAd.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
-import { AdSenseConfig } from '@/app/lib/api';
+import { AdSenseConfig } from '../lib/api';
 
 interface AdSenseAdProps {
   config: AdSenseConfig;
